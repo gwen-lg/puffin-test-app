7246dbb414b913467ef0118f476d67e48aaf20a1
Gwen Lg <me@gwenlg.fr>
2022-06-01 09:56:13 +0200

    add loop behavior management

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use rand::random;
 use std::{thread, time};
 
+#[derive(Clone, Copy)]
+enum LoopBehavior {
+	Unlimited,
+	Limited(u32),
+}
+
 fn main() {
 	let server_addr = format!("0.0.0.0:{}", puffin_http::DEFAULT_PORT);
 	eprintln!("Serving demo profile data on {}", server_addr);
@@ -8,8 +14,10 @@ fn main() {
 
 	puffin::set_scopes_on(true); // need this to enable capture
 
+	let loop_behavior = LoopBehavior::Unlimited;
+
 	let mut loop_count = 0;
-	loop {
+	while continue_loop(loop_behavior, loop_count) {
 		puffin::profile_scope!("main_loop");
 		puffin::GlobalProfiler::lock().new_frame();
 		loop_count += 1;
@@ -29,4 +37,12 @@ fn compute_loop_duration() -> u64 {
 	const MAX_DURATION: u64 = 55;
 	let rand_value: u64 = random::<u64>() % (MAX_DURATION - MIN_DURATION);
 	MIN_DURATION + rand_value
+}
+
+fn continue_loop(loop_behavior: LoopBehavior, loop_count: u32) -> bool {
+	let continue_loop = match loop_behavior {
+		LoopBehavior::Unlimited => true,
+		LoopBehavior::Limited(limit) => loop_count < limit,
+	};
+	continue_loop
 }
