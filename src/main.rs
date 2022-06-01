af3bc137e7d7385367cf604aeab6f218c6895ea6
Gwen Lg <me@gwenlg.fr>
2022-06-01 09:42:12 +0200

    Add puffin marker for loop

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@ use rand::random;
 use std::{thread, time};
 
 fn main() {
+	puffin::set_scopes_on(true); // need this to enable capture
+
 	let mut loop_count = 0;
 	loop {
+		puffin::profile_scope!("main_loop");
+		puffin::GlobalProfiler::lock().new_frame();
 		loop_count += 1;
 
 		let loop_duration = compute_loop_duration();
-		let sleep_duration = time::Duration::from_millis(loop_duration);
-		thread::sleep(sleep_duration);
+		{
+			puffin::profile_scope!("sleep");
+			let sleep_duration = time::Duration::from_millis(loop_duration);
+			thread::sleep(sleep_duration);
+		}
 		println!("loop {} duration {}ms", loop_count, loop_duration);
 	}
 }
