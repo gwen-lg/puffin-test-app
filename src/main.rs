af7725b22960921bad259693356daeb1dedb2807
Gwen Lg <me@gwenlg.fr>
2022-06-01 09:15:16 +0200

    add random for loop duration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::random;
 use std::{thread, time};
 
 fn main() {
@@ -5,9 +6,16 @@ fn main() {
 	loop {
 		loop_count += 1;
 
-		let loop_duration = 33;
+		let loop_duration = compute_loop_duration();
 		let sleep_duration = time::Duration::from_millis(loop_duration);
 		thread::sleep(sleep_duration);
 		println!("loop {} duration {}ms", loop_count, loop_duration);
 	}
+}
+
+fn compute_loop_duration() -> u64 {
+	const MIN_DURATION: u64 = 10;
+	const MAX_DURATION: u64 = 55;
+	let rand_value: u64 = random::<u64>() % (MAX_DURATION - MIN_DURATION);
+	MIN_DURATION + rand_value
 }
