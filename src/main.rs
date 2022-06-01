babaab72934022fac8f69b98f14cbbf88b8e11a1
Gwen Lg <me@gwenlg.fr>
2022-06-01 08:58:08 +0200

    add loop managmenet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{thread, time};
+
 fn main() {
-	println!("Hello, world!");
+	let mut loop_count = 0;
+	loop {
+		loop_count += 1;
+
+		let loop_duration = 33;
+		let sleep_duration = time::Duration::from_millis(loop_duration);
+		thread::sleep(sleep_duration);
+		println!("loop {} duration {}ms", loop_count, loop_duration);
+	}
 }
